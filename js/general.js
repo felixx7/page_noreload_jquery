3ae865c0bf987f9505adf14fa02fea4b17fd3359
Aditya <aditya.radika@gmail.com>
2018-09-16 00:33:36 +0700

    terbaru

--- js/general.js
+++ js/general.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
+	// membaca file
 	$('#content').load('content/home.php');
+	// click ul li a (link)
 	$('ul#nav li a').click(function(){
+		// mengambil atribut href
 		var page = $(this).attr('href');
+		// membaca file folder content 
 		$('#content').load('content/' + page +'.php');
 		return false;
 	});
